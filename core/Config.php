46d71d29f9807a6d4c49fcbc898e59cc7072c11e
jingzhixiang <jingzhixiang@aprbrother.com>
2017-05-15 18:21:29 +0800

    修改swoole 兼容错误

--- core/Config.php
+++ core/Config.php
@@ -3,9 +3,10 @@ namespace Core;
 
 class Config {
     function __construct(){
-        $this->application=include __ROOT__.'/conf/'.'application.php';
+	        $this->application=include __ROOT__.'/conf/'.'application.php';
 		$this->db=include __ROOT__.'/conf/'.'db.php';
 		$this->config=include __ROOT__.'/conf/'.'cache.php';
+		$this->server=include __ROOT__.'/conf/'.'server.php';
     }
     
 }
