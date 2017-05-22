e34c9d58a833f1e959bd0654fadf63df1db288ef
jingzhixiang <jingzhixiang@aprbrother.com>
2017-05-22 16:21:24 +0800

    clean

--- frontend/Index/Index.php
+++ frontend/Index/Index.php
@@ -3,15 +3,11 @@ namespace Frontend\Index;
 
 use Core\Controller;
 use Core\MQ;
-use Core\Pool;
 
 class Index extends Controller {
 	//异步处理列表
 	public $asynchronous=['index'];
 	function index(){
-		$user=(new \Model\User())->where('name=?')->para([
-			11
-		])->get();
-		 var_dump($user);
+
 	}
 }
